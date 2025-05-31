55cc550a81ea647c9fd88c28f186525b6075687e
KhaleghiSadraPC <khaleghibavilsadra@gmail.com>
2025-05-31 09:55:39 -0700

    Add project files.

---
+++ NvidiaFrequencyLocker/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace NvidiaFrequencyLocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            groupBox2 = new GroupBox();
+            label2 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            button1 = new Button();
+            button2 = new Button();
+            toolTip1 = new ToolTip(components);
+            richTextBox1 = new RichTextBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.ForeColor = Color.Black;
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(365, 126);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Min Frequency";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Location = new Point(285, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 41);
+            label1.TabIndex = 1;
+            label1.Text = "MHz";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(16, 56);
+            numericUpDown1.Maximum = new decimal(new int[] { 4000, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 300, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(266, 47);
+            numericUpDown1.TabIndex = 0;
+            numericUpDown1.TextAlign = HorizontalAlignment.Right;
+            numericUpDown1.ThousandsSeparator = true;
+            numericUpDown1.Value = new decimal(new int[] { 300, 0, 0, 0 });
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.Transparent;
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(numericUpDown2);
+            groupBox2.ForeColor = Color.Black;
+            groupBox2.Location = new Point(390, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(366, 126);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Max Frequency";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Location = new Point(286, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 41);
+            label2.TabIndex = 2;
+            label2.Text = "MHz";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(16, 56);
+            numericUpDown2.Maximum = new decimal(new int[] { 4000, 0, 0, 0 });
+            numericUpDown2.Minimum = new decimal(new int[] { 300, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(267, 47);
+            numericUpDown2.TabIndex = 1;
+            numericUpDown2.TextAlign = HorizontalAlignment.Right;
+            numericUpDown2.Value = new decimal(new int[] { 300, 0, 0, 0 });
+            numericUpDown2.ValueChanged += numericUpDown2_ValueChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 155);
+            button1.Name = "button1";
+            button1.Size = new Size(365, 58);
+            button1.TabIndex = 4;
+            button1.Text = "&Set Clocks";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(390, 155);
+            button2.Name = "button2";
+            button2.Size = new Size(366, 58);
+            button2.TabIndex = 5;
+            button2.Text = "&Reset Clocks";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = Color.Black;
+            richTextBox1.Location = new Point(-1, 231);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ScrollBars = RichTextBoxScrollBars.ForcedVertical;
+            richTextBox1.Size = new Size(771, 284);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(768, 512);
+            Controls.Add(richTextBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "NvFreqLock";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private ToolTip toolTip1;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ NvidiaFrequencyLocker/Form1.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace NvidiaFrequencyLocker
+{
+    public partial class Form1 : Form
+    {
+        // Do not include quotes in the path
+        const string PATH2SMI = @"C:\Windows\System32\nvidia-smi.exe";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string min = numericUpDown1.Value.ToString();
+            string max = numericUpDown2.Value.ToString();
+
+            this.RunNvidiaSMI($"-lgc {min},{max}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.RunNvidiaSMI("-rgc");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown2.Minimum = Math.Max(300, numericUpDown1.Value);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = numericUpDown2.Value;
+        }
+
+        int MessegeCount = 0;
+
+        /// <summary>
+        /// Runs nvidia-smi directly without using cmd.exe
+        /// </summary>
+        private void RunNvidiaSMI(string arguments)
+        {
+            try
+            {
+                Process process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = PATH2SMI,
+                        Arguments = arguments,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                process.WaitForExit();
+
+                if (String.IsNullOrWhiteSpace(error))
+                {
+                    LogInvokeResult($">> {output}", Color.Lime);
+                }
+                else
+                {
+                    LogInvokeResult($">> {error}", Color.Red);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogInvokeResult($">> {ex.Message}", Color.Red);
+            }
+        }
+
+        public void LogInvokeResult(string text, Color color)
+        {
+            richTextBox1.SuspendLayout();
+            richTextBox1.SelectionColor = color;
+            richTextBox1.AppendText(text);
+            richTextBox1.ScrollToCaret();
+            richTextBox1.ResumeLayout();
+        }
+    }
+}
